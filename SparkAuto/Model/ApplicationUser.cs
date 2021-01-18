384f8f1dd03c84cd54746b2968c954e38f812532
Taraszovics Károly <tarakarcsi94@gmail.com>
2021-01-18 13:20:28 +0100

    Add properties to User + db migration

---
+++ SparkAuto/Model/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparkAuto.Model
+{
+    public class ApplicationUser :IdentityUser
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string PostalCode { get; set; }
+    }
+}
