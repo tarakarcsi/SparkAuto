408a44931cf03f5bc296e665cd08a2633a8af807
Taraszovics Károly <tarakarcsi94@gmail.com>
2021-01-18 13:02:15 +0100

    Add Details and Delete features

---
+++ SparkAuto/Pages/ServiceTypes/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Model;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly SparkAuto.Data.ApplicationDbContext _db;
+
+        public DetailsModel(SparkAuto.Data.ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public ServiceType ServiceType { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ServiceType = await _db.ServiceType.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (ServiceType == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
