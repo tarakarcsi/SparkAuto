b5c2a0283a216d109883812146b2ea2b495b177f
Taraszovics Károly <tarakarcsi94@gmail.com>
2021-01-18 10:41:40 +0100

    Add Create view UI

---
+++ SparkAuto/Pages/ServiceTypes/Create.cshtml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SparkAuto.Model;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class CreateModel : PageModel
+    {
+        public ServiceType ServiceType { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
