5bb368a837554ad84817a8b0cf346bb03a7cf996
Taraszovics Károly <tarakarcsi94@gmail.com>
2021-01-20 12:26:14 +0100

    Add User List Page

---
+++ SparkAuto/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SparkAuto.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public List<ApplicationUser> ApplicationUserList { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            ApplicationUserList = await _db.ApplicationUser.ToListAsync();
+
+            return Page();
+        }
+    }
+}
