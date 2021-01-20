898169f8ae44a5bdb24118f27d4823a0a3831bbd
Taraszovics Károly <tarakarcsi94@gmail.com>
2021-01-20 13:25:45 +0100

    Add Paging #1

--- SparkAuto/Pages/Users/Index.cshtml.cs
+++ SparkAuto/Pages/Users/Index.cshtml.cs
@@ -3,7 +3,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using SparkAuto.Data;
 using SparkAuto.Model;
+using SparkAuto.Model.ViewModel;
+using SparkAuto.Utility;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SparkAuto.Pages.Users
@@ -18,11 +22,33 @@ namespace SparkAuto.Pages.Users
         }
 
         [BindProperty]
-        public List<ApplicationUser> ApplicationUserList { get; set; }
+        public UsersListViewModel UsersListVM { get; set; }
 
-        public async Task<IActionResult> OnGet()
+        public async Task<IActionResult> OnGet(int productPage = 1)
         {
-            ApplicationUserList = await _db.ApplicationUser.ToListAsync();
+            UsersListVM = new UsersListViewModel()
+            {
+                ApplicationUserList = await _db.ApplicationUser.ToListAsync()
+            };
+
+            StringBuilder param = new StringBuilder();
+            param.Append("/Users?productPage=:");
+
+            var count = UsersListVM.ApplicationUserList.Count;
+
+            UsersListVM.PagingInfo = new PagingInfo
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = SD.PaginationUsersPageSize,
+                TotalItems = count,
+                UrlParam = param.ToString()
+            };
+
+            UsersListVM.ApplicationUserList = UsersListVM.ApplicationUserList
+                .OrderBy(p => p.Email)
+                .Skip((productPage - 1) * SD.PaginationUsersPageSize)
+                .Take(2)
+                .ToList();
 
             return Page();
         }
